7e7717bf1d0e795949bf5ca79b5be23974d76efa
XAV42190 <xvadon@gmail.com>
2026-07-21 14:25:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrément de version pour forcer la purge du cache mobile
-const CACHE_NAME = 'livetrack-v503';
+const CACHE_NAME = 'livetrack-v600';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
